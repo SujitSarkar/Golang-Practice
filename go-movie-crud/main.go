6f250fce11db9e39506ca0c3b0708488b79da7f9
Sujit Sarkar <snigdho532@gmail.com>
2022-12-15 17:39:49 +0600

    movie-crud main.go started

--- go-movie-crud/main.go
+++ go-movie-crud/main.go
@@ -1,3 +1,35 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"direstor"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/movies", getMoview).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", creatMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Println("Starting server at post 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
